2df5ef2c5eb8e508a4f8710fcf6f708e5c6a48de
JIASHUANG YIN <jiashuangyin@JIASHUANGdeMacBook-Pro.local>
2021-06-07 23:30:21 +0800

    tabbar部分

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 4.懒加载路由界面
+const home=()=>import('../views/home/home.vue')
+const category=()=>import('../views/category/category.vue')
+const shopcart=()=>import('../views/shopcart/shopcart.vue')
+const profile=()=>import('../views/profile/profile.vue')
+
+// 1.安装插件
+Vue.use(Router)
+
+
+// 2.创建路由对象
+const routes=[
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:home
+  },
+  {
+    path:'/category',
+    component:category
+  },
+  {
+    path:'/shopcart',
+    component:shopcart
+  },
+  {
+    path:'/profile',
+    component:profile
+  }
+
+]
+
+const router=new Router({
+  routes,
+  // 让网址看起来更加简介，不用hash模式，要用history模式
+  mode:'history'
+})
+// 3.new完以后导出
+export default router
